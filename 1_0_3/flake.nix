c0aee2a6c4fdbd9ff2c6b229b3cc37ed073620b5
hugosenari <hugosenari@gmail.com>
2022-03-12 00:36:44 -0300

    chore: re index atoz

---
+++ 1_0_3/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''Amazon Web Services (AWS) APIs'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-atoz-1_0_3.flake = false;
+  inputs.src-atoz-1_0_3.owner = "disruptek";
+  inputs.src-atoz-1_0_3.ref   = "refs/tags/1.0.3";
+  inputs.src-atoz-1_0_3.repo  = "atoz";
+  inputs.src-atoz-1_0_3.type  = "github";
+  
+  inputs."openapi".dir   = "nimpkgs/o/openapi";
+  inputs."openapi".owner = "riinr";
+  inputs."openapi".ref   = "flake-pinning";
+  inputs."openapi".repo  = "flake-nimble";
+  inputs."openapi".type  = "github";
+  inputs."openapi".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openapi".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."sigv4".dir   = "nimpkgs/s/sigv4";
+  inputs."sigv4".owner = "riinr";
+  inputs."sigv4".ref   = "flake-pinning";
+  inputs."sigv4".repo  = "flake-nimble";
+  inputs."sigv4".type  = "github";
+  inputs."sigv4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sigv4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-atoz-1_0_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-atoz-1_0_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
